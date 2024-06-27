c697f2a79cc450ba998bfb659a90f6efbba78741
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-06-27 09:33:50 -0300

    feat: adds home component

---
+++ components/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Image } from 'primereact/image';
+import { Card } from 'primereact/card';
+import '../node_modules/primeflex/primeflex.css';
+import 'primereact/resources/themes/arya-blue/theme.css';
+import Header from './Header';
+
+export default function Home() {
+    const url = '/rick-and-morty-header.jpg'
+
+    return (
+        <>
+            <Header />
+            <div className='homeImage w-full flex flex-column mt-5 lign-items-center justify-content-center'>
+                <Card className='mb-2 bg-cyan-500'>
+                    <div className='flipup animation-duration-3000 animation-iteration-infinite animation-ease-in-out'>
+                        <h1>Welcome to Rick and Morty Wiki</h1>
+                    </div>
+                    <p>Discover more about the characters, episodes and locations of the Rick and Morty series.</p>
+                </Card>
+                <Image src={url} alt={"Rick and Morty header"} width={'100%'}>
+                    Olá
+                </Image>
+            </div>
+
+        </>
+    );
+}
