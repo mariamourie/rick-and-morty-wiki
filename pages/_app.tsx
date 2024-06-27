c049479e763a2a49c1374676dc6574057624707c
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-06-27 09:41:43 -0300

    refactor: adds new theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import Home from './index';
-import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import '../node_modules/primeflex/primeflex.css';
+import 'primereact/resources/themes/arya-blue/theme.css';
 
 // COMPONENTS
-import Header from "@/components/Header";
-import Episodes from './Episodes';
-import Location from './Location'
-import Character from './Characters';
+import Home from '@/components/Home';
 
-export default function Home() {
+export default function App() {
   return (
     <>
-      <Header />
-      <div className='flex flex-column justify-content-center'>
-        <Character />
-      </div>
+      <Home />
     </>
   );
 }
