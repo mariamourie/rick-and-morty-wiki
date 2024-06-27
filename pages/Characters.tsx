8e58ca7bc65e700fc49a026c9a2f05774ef0735f
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-06-27 09:40:43 -0300

    feat: adds breadcrumb

--- pages/Characters.tsx
+++ pages/Characters.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
+
+import Header from "@/components/Header";
 import Cards from '@/components/Card';
+
 import { Paginator } from 'primereact/paginator';
 import { Toolbar } from 'primereact/toolbar';
 import { Dropdown } from 'primereact/dropdown';
-import Service from '../services/characters_services';
+import { BreadCrumb } from 'primereact/breadcrumb';
+
+import 'primereact/resources/themes/arya-blue/theme.css';
+
+import Service from '../services/characters_service';
+
 import C from '../utils/constants';
 
 export default function Character() {
@@ -30,7 +38,6 @@ export default function Character() {
     }, [updateFetchedData]);
 
     const api = () => {
-        console.log(params)
         if (selectedGender == null && selectedStatus == null) {
             service
                 .getAllCharacters(pagesNumber)
@@ -39,8 +46,11 @@ export default function Character() {
                 });
         }
         else {
+            if (selectedGender != null && selectedStatus != null) {
+                params = `gender=${selectedGender}&status=${selectedStatus}`
+            }
             service
-                .getCharactersBy(params, pagesNumber)
+                .getCharactersByParams(params, pagesNumber)
                 .then((response) => {
                     updateFetchedData(response);
                 });
@@ -50,26 +60,23 @@ export default function Character() {
 
     results = fetchedData['results'];
     info = fetchedData['info'];
-    let { count, pages } = info;
 
     const onPageChange = (event) => {
-        pagesNumber = event.page + 1;
-        api();
-        update(fetchedData['info'], fetchedData['results']);
         setFirst(event.first);
+        pagesNumber = event.page + 1;
+        console.log(params)
+        api();
     };
 
     const onChangeGenderValue = (event) => {
         setSelectedGender(event.value);
-        paramsFilter = selectedStatus == null ? '' : `status=${selectedStatus}`;
-        params = `gender=${event.value}&${paramsFilter}`;
+        params += `gender=${event.value}&`;
         api();
     }
 
     const onChangeStatusValue = (event) => {
         setSelectedStatus(event.value);
-        paramsFilter = selectedGender == null ? '' : `gender=${selectedGender}&`;
-        params = `status=${event.value}&${paramsFilter}`;
+        params += `status=${event.value}&`;
         api();
     }
 
@@ -83,13 +90,35 @@ export default function Character() {
             options={C.STATUS} placeholder='Select a status' className='w-full md:w-14rem' />
     )
 
+    const items = [
+        {
+            label: 'Episodes'
+        }
+    ]
+    const home = {
+        icon: 'pi pi-home',
+        command: (e) => {
+            router.push('../')
+        }
+    }
+
     return (
-        <div className='flex flex-column justify-content-center align-items-center'>
-            <Toolbar className='m-3 w-8 bg-yellow-400 border-round-3xl' start={genderToolBar} center={statusToolBar} />
-            <div className='flex flex-wrap justify-content-center'>
-                <Cards results={results} />
+        <>
+            <Header />
+            <BreadCrumb className='w-full bg-cyan-500' model={items} home={home} />
+            <div className='flex flex-column flew-wrap justify-content-center align-items-center'>
+                <div className='mt-4 mb-4 flex justify-content-center align-items-center'>
+                    <i className='pi pi-filter mr-3' />
+                    <Dropdown value={selectedGender} onChange={onChangeGenderValue}
+                        options={C.GENDERS} placeholder='Select a gender' className='w-full md:w-14rem mr-3' />
+                    <Dropdown value={selectedStatus} onChange={onChangeStatusValue}
+                        options={C.STATUS} placeholder='Select a status' className='w-full md:w-14rem' />
+                </div>
+                <div className='flex flex-wrap justify-content-center'>
+                    <Cards results={results} />
+                </div>
+                <Paginator first={first} rows={20} totalRecords={826} onPageChange={onPageChange} template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} />
             </div>
-            <Paginator first={first} rows={20} totalRecords={count} onPageChange={onPageChange} template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} />
-        </div>
+        </>
     )
 }
