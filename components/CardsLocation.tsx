1a02bc8f70154e6e308c423c369fdaa01377fbd6
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-06-28 19:18:42 -0300

    refactor: changes the key card

--- components/CardsLocation.tsx
+++ components/CardsLocation.tsx
@@ -6,10 +6,10 @@ export default function CardLocation({ results }) {
     let display;
 
     if (results) {
-        display = results.map((item) => {
+        display = results.map((item, index) => {
             return (
-                <div className="p-card w-5 m-2">
-                    <Card className="p-2 w-full flex flex-column flex-wrap" key={item.id}>
+                <div className="p-card w-5 m-2" key={item.id}>
+                    <Card className="p-2 w-full flex flex-column flex-wrap">
                         <h1 className="text-cyan-600 font-bold">{item.name}</h1>
                         <p className="flex flex-column"><span className="text-gray-500 font-bold mb-2">Dimension </span>{item.dimension}</p>
                         <p className="flex flex-column"><span className="text-gray-500 font-bold mb-2">Type</span> {item.type}</p>
--- components/EpisodesCard.tsx
+++ components/EpisodesCard.tsx
@@ -5,13 +5,13 @@ export default function EpisodeCards({ results }) {
     let display;
 
     if (results) {
-        display = results.map((item) => {
+        display = results.map((item, index) => {
 
             const { id, name, air_date, episode } = item;
 
             return (
-                <div className='p-card w-5 m-2'>
-                    <Card className='w-full flex flex-column flex-wrap' key={id}>
+                <div className='p-card w-5 m-2' key={id}>
+                    <Card className='w-full flex flex-column flex-wrap'>
                         <p className='flex flex-column'><span className="mb-2 font-bold text-cyan-500">Name</span>{name}</p>
                         <p className='flex flex-column'><span className="mb-2 font-bold text-cyan-500">Air Date</span>{air_date}</p>
                         <p className='flex flex-column'><span className="mb-2 font-bold text-cyan-500">Episode</span>{episode}</p>
