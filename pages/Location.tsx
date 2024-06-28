64c1ff4c0e4e132b729aee0b742f3199f8c5153f
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-06-28 18:40:49 -0300

    refactor: removes the filter

--- pages/Location.tsx
+++ pages/Location.tsx
@@ -18,12 +18,8 @@ export default function Location() {
 
     let pagesNumber = 1;
     let info, results;
-    let types = [];
-    let params = '';
 
     const service = new Service();
-
-    const [type, setType] = useState(null);
 
     const [first, setFirst] = useState(0);
     const [fetchedData, updateFetchedData] = useState([]);
@@ -33,29 +29,15 @@ export default function Location() {
     }, [updateFetchedData]);
 
     const api = () => {
-
-        if (params == '') {
-            service
-                .getAllLocations(pagesNumber)
-                .then((response) => {
-                    updateFetchedData(response);
-                });
-        } else {
-            if (type != null) {
-                params = `type=${type}`;
-            }
-            service.getLocationByParams(pagesNumber, params).then((response) =>
-                updateFetchedData(response));
-            updateData(fetchedData['info'], fetchedData['results']);
-        }
+        service
+            .getAllLocations(pagesNumber)
+            .then((response) => {
+                updateFetchedData(response);
+            });
     }
 
     results = fetchedData['results'];
     info = fetchedData['info'];
-
-    if (results != undefined) {
-        types = getAllTypes(results);
-    }
 
     const items = [
         {
@@ -73,26 +55,12 @@ export default function Location() {
         api();
         setFirst(event.first);
     }
-    const onClick = (event) => {
-        setType(null);
-        api();
-    }
-
-    const onChangeTypeValue = (event) => {
-        setType(event.value);
-        params += `type=${event.value}&`;
-        api();
-    }
 
     return (
         <div className='w-full'>
             <Header />
             <Breadcrumb items={items} />
-            <div className='flex justify-content-center mt-5 mb-5 align-items-center'>
-                <Button className='bg-cyan-500 text-white-alpha-90' label='X' onClick={onClick} />
-                <Dropdown className='mr-2 ml-2' value={type} onChange={onChangeTypeValue} options={types} placeholder='Select a type' />
-            </div>
-            <div className='flex flex-wrap justify-content-center align-items-center'>
+            <div className='mt-5 flex flex-wrap justify-content-center align-items-center'>
                 <CardLocation results={results} />
             </div>
             <Pagination first={first} info={info} onPageChange={onPageChange} />
