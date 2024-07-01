b62609d9d7bef37404295107cbc7a15aca1c45ce
mariamourie <mariaeduarda.empathy@gmail.com>
2024-07-01 18:11:20 -0300

    refactor: adds typing to the parameters

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Paginator } from 'primereact/paginator';
+import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator';
 
-const Pagination = ({ first, info, onPageChange }) => {
-    if (info) {
-        const { count, pages } = info;
+const Pagination = (props: { info: { count: number, pages: number }, onPageChange: (event: PaginatorPageChangeEvent) => void, first: number }) => {
+    if (props.info) {
+        const { count, pages } = props.info;
         const rows = Math.round(count / pages);
         return (
-            <Paginator className='mt-5' first={first} rows={rows} totalRecords={count} onPageChange={onPageChange} template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} />
+            <Paginator className='mt-5' first={props.first} rows={rows} totalRecords={count} onPageChange={props.onPageChange} template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} />
         )
     }
 }
